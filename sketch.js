280aec78b0bc8376bbf4a40ba28c47abc35368d4
shivsundergour <81570170+shivsundergour@users.noreply.github.com>
2021-06-21 19:19:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var ground,ball;
+var binImg,bin;
+
+function preload(){
+    binImg = loadImage("dustbingreen.png");
+}
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground1 = new Ground();
+    crumpledPaper = new paper();
+
+    bin = createSprite(964,500,20,20);
+    bin.addImage(binImg);
+    bin.scale = 0.53;
+
+    binPart1 = new Dustbin(902,505,10,120);
+    binPart2 = new Dustbin(962,565,130,10);
+    binPart3 = new Dustbin(1024,505,10,120);
+}
+
+function draw(){
+    background(222);
+    Engine.update(engine);
+
+    //text(mouseX+","+mouseY,200,200);
+
+    
+    ground1.display();
+    crumpledPaper.display();
+    binPart1.display();
+    binPart2.display();
+    binPart3.display(); 
+      
+    drawSprites();
+}
+
+function keyPressed(){
+    if(keyCode === UP_ARROW){
+        Matter.Body.applyForce(crumpledPaper.body,crumpledPaper.body.position,{x:74,y:-75});
+    }
+}
